9c769924abd6083654eb118e719ebd8f3b939e84
regibayoan <regi.bayoan@gmail.com>
2020-04-25 08:03:23 +0100

    Fixed error of showing success message

--- startRobot.py
+++ startRobot.py
@@ -8,7 +8,7 @@ eel.init('web')
 @eel.expose
 def set_waypoint(data1,data2,data3,data4,data5,data6):
     # Send data to Raspberry Pi
-    return 0
+    return 1
 
 
 eel.start('gui.html', size=(1000, 800))
